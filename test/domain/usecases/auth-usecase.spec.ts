723824b3e71f7b13a930ade50b3e99c83bb74c96
henricker <henriquevieira@alu.ufc.br>
2022-06-17 23:49:19 -0300

    test: should throw if userRepository throws

--- test/domain/usecases/auth-usecase.spec.ts
+++ test/domain/usecases/auth-usecase.spec.ts
@@ -1,7 +1,7 @@
-import { IUserRepository } from '../../../src/domain/repositories/user-repository.interface';
-import { AuthUseCase } from '../../../src/domain/usecases/authentication/auth-usecase';
-import { mockAuthenticationParams } from '../../mocks/mock-account';
-import { makeUserRepository } from '../../mocks/repositories/mock-user.repository';
+import { IUserRepository } from "../../../src/domain/repositories/user-repository.interface";
+import { AuthUseCase } from "../../../src/domain/usecases/authentication/auth-usecase";
+import { mockAuthenticationParams } from "../../mocks/mock-account";
+import { makeUserRepository } from "../../mocks/repositories/mock-user.repository";
 
 type SutType = {
   sut: AuthUseCase;
@@ -18,24 +18,36 @@ const makeSut = (): SutType => {
   };
 };
 
-describe('#UseCase auth', () => {
-  it('Should call repository with correct values', async () => {
+describe("#UseCase auth", () => {
+  it("Should call repository with correct values", async () => {
     const { repositoryStub, sut } = makeSut();
-    const repositorySpy = jest.spyOn(repositoryStub, 'findOneByEmail');
+    const repositorySpy = jest.spyOn(repositoryStub, "findOneByEmail");
     const authParams = mockAuthenticationParams();
     sut.handle(authParams);
     expect(repositorySpy).toBeCalledWith(authParams.email);
   });
 
-  it('Should return null if email not exists', async () => {
+  it("Should return null if email not exists", async () => {
     const { repositoryStub, sut } = makeSut();
 
     jest
-      .spyOn(repositoryStub, 'findOneByEmail')
+      .spyOn(repositoryStub, "findOneByEmail")
       .mockImplementationOnce(() => null);
 
     const received = await sut.handle(mockAuthenticationParams());
 
     expect(received).toBeNull();
+  });
+
+  it("Should throw if repository throws", async () => {
+    const { repositoryStub, sut } = makeSut();
+
+    jest.spyOn(repositoryStub, "findOneByEmail").mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const received = sut.handle(mockAuthenticationParams());
+
+    expect(received).rejects.toThrow();
   });
 });
