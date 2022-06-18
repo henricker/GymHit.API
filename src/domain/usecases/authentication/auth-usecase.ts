c0158011bb361f3aabccfe5ca8eb54f28dc4f9f0
henricker <henriquevieira@alu.ufc.br>
2022-06-17 23:50:52 -0300

    refactor: using params protocol instead of type on own file

--- src/domain/usecases/authentication/auth-usecase.ts
+++ src/domain/usecases/authentication/auth-usecase.ts
@@ -1,22 +1,18 @@
-import { IUserRepository } from '../../repositories/user-repository.interface';
-import { IUseCase } from '../usecase.interface';
-
-type AuthUseCaseDto = {
-  email: string;
-  password: string;
-};
+import { IUserRepository } from "../../repositories/user-repository.interface";
+import { IUseCase } from "../usecase.interface";
+import { IAuthParams } from "./auth-params.dto";
 
 export class AuthUseCase implements IUseCase {
   constructor(private readonly repository: IUserRepository) {}
 
   /* eslint-disable no-unused-vars */
-  async handle({ email, password }: AuthUseCaseDto): Promise<string | null> {
+  async handle({ email, password }: IAuthParams): Promise<string | null> {
     const user = await this.repository.findOneByEmail(email);
 
     if (!user) {
       return null;
     }
 
-    return 'access_token';
+    return "access_token";
   }
 }
